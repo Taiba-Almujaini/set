2a480675029858949510305e235acd8c0b0b56fa
Taiba-Almujaini <almujaini1995.t@gmail.com>
2024-02-26 12:20:41 +0400

    Create a Set, add some elements to it, and then remove all elements from the Set. Verify it's empty by printing its size.

---
+++ Clear a Set.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Set<Integer> newSet = new HashSet<>();
+        newSet.add(1);
+        newSet.add(2);
+        newSet.add(3);
+        newSet.add(4);
+
+
+        System.out.println("Size of the set: " + newSet.size());
+        newSet.clear();
+        System.out.println("Size after clear: " + newSet.size());
+
+
+
+
+    }
+}
