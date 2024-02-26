8a73226cedc5a914c727a0916a77817cb72653fd
Taiba-Almujaini <almujaini1995.t@gmail.com>
2024-02-26 12:16:59 +0400

    create a Set, add elements to it, and print the entire Set.

---
+++ Create and Populate a Set.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Set<Integer> newSet = new HashSet<>();
+        newSet.add(1);
+        newSet.add(2);
+        newSet.add(3);
+        newSet.add(4);
+
+
+        System.out.println("Set :"+newSet );
+
+
+
+    }
+}
