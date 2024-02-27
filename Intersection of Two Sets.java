71dab8c78cb8b74b28246d41f58c274ace6ba0f8
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-02-27 12:01:02 +0400

    Intersection of Two Sets.java

--- Intersection of Two Sets.java
+++ Intersection of Two Sets.java
@@ -2,7 +2,7 @@
 import java.util.HashSet;
 import java.util.Arrays;
 import java.util.Set;
-public class main{
+public class IntersectionTwoSets{
     public static void main(String[] args) {
 
         HashSet<Integer> set1 = new HashSet<Integer>();
@@ -11,12 +11,12 @@ public class main{
         HashSet<Integer> set2 = new HashSet<Integer>();
         set2.addAll(Arrays.asList(17, 20, 32, 4,51));
 
-        HashSet<Integer> intersection = new HashSet<>(set1);// Create a new HashSet to store the intersection
+        HashSet<Integer> Intersection = new HashSet<>(set1);// Create a new HashSet to store the intersection
 
-        intersection.retainAll(set2);
-		
+        Intersection.retainAll(set2);
+
         System.out.println("Intersection of set1 and set2:");  // Print the intersection
-        System.out.println(intersection);
+        System.out.println(Intersection);
     }
 
 
