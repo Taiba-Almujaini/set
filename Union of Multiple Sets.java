eeff67b8ed1f3ae8861100893e9d32794f1cb72d
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-02-28 10:13:49 +0400

    UnionOfSets.java

--- Union of Multiple Sets.java
+++ Union of Multiple Sets.java
@@ -1,30 +1,30 @@
 
 import java.util.HashSet;
 import java.util.Arrays;
-import java.util.Set;
-public class main{
+
+public class UnionOfSets {
     public static void main(String[] args) {
 
-        HashSet<Integer> set1 = new HashSet<Integer>();
-        set1.addAll(Arrays.asList(5, 20, 53, 4,5));
+        HashSet<Integer> firstSet = new HashSet<Integer>();
+        firstSet.addAll(Arrays.asList(5, 20, 53, 4,5));
 
-        HashSet<Integer> set2 = new HashSet<Integer>();
-        set2.addAll(Arrays.asList(17, 20, 32, 4,51));
+        HashSet<Integer> secondSet = new HashSet<Integer>();
+        secondSet.addAll(Arrays.asList(17, 20, 32, 4,51));
 
-        HashSet<Integer> set3 = new HashSet<Integer>();
-        set3.addAll(Arrays.asList(20, 5, 32, 4,11));
-
-
-        HashSet<Integer> union = new HashSet<>();// Create a new HashSet to store the union
+        HashSet<Integer> thirdSet = new HashSet<Integer>();
+        thirdSet.addAll(Arrays.asList(20, 5, 32, 4,11));
 
 
-        union.addAll(set1);// Add all elements from set1 to the union
-        union.addAll(set2);// Add all elements from set2 to the union
-        union.addAll(set3);// Add all elements from set3 to the union
+        HashSet<Integer> unionSet = new HashSet<>();// Create a new HashSet to store the union
+
+
+        unionSet.addAll(firstSet);// Add all elements from set1 to the union
+        unionSet.addAll(secondSet);// Add all elements from set2 to the union
+        unionSet.addAll(thirdSet);// Add all elements from set3 to the union
 
         // Print the union
         System.out.println("union of set1 ,set2 and set3:");
-        System.out.println(union);
+        System.out.println(unionSet);
     }
 
 
