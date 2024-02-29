07b864aba10043c5e46d8ac1a5bd96ffbc36a7e8
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-02-29 09:25:55 +0400

    FindDuplicateNumber.java

--- FindDuplicateNumber.java
+++ FindDuplicateNumber.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class FindDuplicateNumber {
     public static void main(String[] args) {
         int[] numbers = {3,3,2,8,6,4,5,2,};
-        Set<Integer> NumberSet = new HashSet<>(); //set for store array numbers
+        Set<Integer> numberSet = new HashSet<>(); //set for store array numbers
         Set<Integer> duplicates = new HashSet<>();// set for store duplicate numbers in array
 
         for (int n : numbers) {
             // If add() returns false, it means the element is already present in the set
-            if (!NumberSet.add(n)) {
+            if (!numberSet.add(n)) {
                 duplicates.add(n);
             }
         }
