985fc1d386bb6a17eb8b7c0335f905ea9d54016a
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-02-29 09:06:18 +0400

    find the duplicate number in an array with n+1 integers, each between 1 and n.

---
+++ FindDuplicateNumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class FindDuplicateNumber {
+    public static void main(String[] args) {
+        int[] numbers = {3,3,2,8,6,4,5,2,};
+        Set<Integer> NumberSet = new HashSet<>(); //set for store array numbers
+        Set<Integer> duplicates = new HashSet<>();// set for store duplicate numbers in array
+
+        for (int n : numbers) {
+            // If add() returns false, it means the element is already present in the set
+            if (!NumberSet.add(n)) {
+                duplicates.add(n);
+            }
+        }
+
+        if (duplicates.isEmpty()) {
+            System.out.println("No duplicates found");
+        } else {
+            System.out.println("Duplicates found: " + duplicates);
+        }
+    }
+}
