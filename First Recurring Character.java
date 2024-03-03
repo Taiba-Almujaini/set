ad5cb52491d631eede3405237307800cdee80097
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-03-03 10:11:12 +0400

    Find the first character that appears more than once in a string.

---
+++ First Recurring Character.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class FirstRecurringCharacter{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: "); // ask uer to enter string
+        String input = scanner.nextLine();
+
+        Set<Character> characterSet = new HashSet<>();// create set
+
+        for (char character : input.toCharArray()) {
+            if (characterSet.contains(character)) { // check if contain same character
+                System.out.println("First recurring character: " + character);
+                break;
+            } else {
+                characterSet.add(character);
+            }
+        }
+
+    }
+}
